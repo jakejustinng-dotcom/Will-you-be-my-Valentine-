e938603a229d8bae9084078287e575e26459f031
jakejustinng-dotcom <jakejustin.ng@gmail.com>
2026-01-28 20:43:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
+    "Are you sure bebikow?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "lablab please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please! ❤️ parang awa mo na :>"
 ];
 
 let messageIndex = 0;
